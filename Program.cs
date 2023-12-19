75202e39458dc2aa4a3f7c61a91b8dca1756a4ec
Thays Rodrigues <thays.avr@gmail.com>
2023-12-18 23:38:37 -0300

    Desafio dos smartphones entregue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia(numero: "999999999", modelo: "Nokia G21", imei: "1234", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo(nomeApp: "Facebook");
+
+Iphone iphone = new Iphone(numero: "988888888", modelo: "Iphone 12", imei: "5678", memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo(nomeApp: "Instagram");
